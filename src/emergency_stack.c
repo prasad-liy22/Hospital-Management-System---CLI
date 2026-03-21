e9ba119cc6a3647565fa070917c52a3b1b96b524
Amaya-Senadheera <amayasenadheera6@gmail.com>
2026-03-21 13:40:41 +0530

    Update emergency_stack.c

---
+++ src/emergency_stack.c
@@ -0,0 +1,74 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+struct Patient { 
+int id; 
+char name [50]; 
+struct Patient* next; 
+}; 
+struct Patient* top = NULL; 
+//Add Emergency Patient (Push) 
+void add_imergency_patient() { 
+struct Patient* newNode = (struct Patient*)malloc(sizeof(struct 
+Patient)); 
+printf("Enter Patient ID: "); 
+scanf("%d", &newNode->id); 
+printf("Enter Patient Name: "); 
+scanf("%s", newNode->name); 
+newNode->next = top; 
+top = newNode; 
+printf("Emergency patient added!\n"); 
+} 
+//Treat Patient (Pop) 
+void treat_patient() { 
+if (top == NULL) { 
+printf("No emergency patients!\n"); 
+} else { 
+struct Patient* temp = top; 
+printf("Treating Patient: %d - %s\n", temp->id, temp->name); 
+top = top->next; 
+free(temp); 
+} 
+} 
+//Display  
+void display() { 
+struct Patient* temp = top; 
+if (temp == NULL) { 
+printf("No emergency patients\n"); 
+} else { 
+while (temp != NULL) { 
+printf("%d - %s\n", temp->id, temp->name); 
+temp = temp->next; 
+} 
+} 
+} 
+//Main Function  
+int main() { 
+int choice; 
+do { 
+printf("\n--- Emergency Stack Menu ---\n"); 
+printf("1. Add Emergency Patient (Push)\n"); 
+printf("2. Treat Patient (Pop)\n"); 
+printf("3. Display Emergency Patients\n"); 
+printf("4. Exit\n"); 
+printf("Enter your choice: "); 
+scanf("%d", &choice); 
+switch(choice) { 
+case 1: 
+add_imergency_patient(); 
+break; 
+case 2: 
+treat_patient(); 
+break; 
+case 3: 
+display(); 
+break; 
+case 4: 
+printf("Exiting...\n"); 
+break; 
+default: 
+printf("Invalid choice!\n"); 
+} 
+} while(choice != 4); 
+return 0; 
+}
